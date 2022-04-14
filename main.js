5e38481249c228303c30d0e8deef218d2a2e3186
Mouad Louhibi <mouadlouhibi2000@gmail.com>
2022-04-14 00:04:35 +0000

    Final

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let container = document.querySelector(".container");
-// let btn = document.getElementById("spin");
+let btn = document.getElementById("spin");
 let number = Math.ceil(Math.random() * 1000);
 
-/*btn.onclick = */
-function click() {
+btn.onclick =  function () {
     container.style.transform = "rotate(" + number + "deg)";
     number += Math.ceil(Math.random() * 1000);
 }
